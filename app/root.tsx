dc1d7551f5ec0cbcf8572491075ac3e08a517a4e
Akash Agarwal <akash191095@gmail.com>
2023-04-28 22:43:06 +0530

    Setup themes with dark mode

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, LoaderFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,30 +7,127 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
-
+import { ChakraProvider, cookieStorageManagerSSR } from "@chakra-ui/react";
+import theme from "./theme";
 import tailwindStylesheetUrl from "~/styles/tailwind.css";
+import { withEmotionCache } from "@emotion/react";
+import { ServerStyleContext, ClientStyleContext } from "./context";
+import { useContext, useEffect, useMemo } from "react";
 
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: tailwindStylesheetUrl },
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+interface DocumentProps {
+  children: React.ReactNode;
+}
 
-export default function App() {
+export let links: LinksFunction = () => {
+  return [
+    { rel: "stylesheet", href: tailwindStylesheetUrl },
+    ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+    { rel: "preconnect", href: "https://fonts.googleapis.com" },
+    { rel: "preconnect", href: "https://fonts.gstatic.com" },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap",
+    },
+  ];
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  // first time users will not have any cookies and you may not return
+  // undefined here, hence ?? is necessary
+  return request.headers.get("cookie") ?? "";
+};
+
+function getColorMode(cookies: string) {
+  const match = cookies.match(
+    new RegExp(`(^| )${CHAKRA_COOKIE_COLOR_KEY}=([^;]+)`)
+  );
+  return match == null ? void 0 : match[2];
+}
+
+// here we can set the default color mode. If we set it to null,
+// there's no way for us to know what is the the user's preferred theme
+// so the cient will have to figure out and maybe there'll be a flash the first time the user visits us.
+const DEFAULT_COLOR_MODE: "dark" | "light" | null = "dark";
+
+const CHAKRA_COOKIE_COLOR_KEY = "chakra-ui-color-mode";
+
+const App = withEmotionCache(({ children }: DocumentProps, emotionCache) => {
+  const serverStyleData = useContext(ServerStyleContext);
+  const clientStyleData = useContext(ClientStyleContext);
+
+  // Only executed on client
+  useEffect(() => {
+    // re-link sheet container
+    emotionCache.sheet.container = document.head;
+    // re-inject tags
+    const tags = emotionCache.sheet.tags;
+    emotionCache.sheet.flush();
+    tags.forEach((tag) => {
+      (emotionCache.sheet as any)._insertTag(tag);
+    });
+    // reset cache to reapply global styles
+    clientStyleData?.reset();
+  }, []);
+
+  let cookies = useLoaderData();
+
+  // the client get the cookies from the document
+  // because when we do a client routing, the loader can have stored an outdated value
+  if (typeof document !== "undefined") {
+    cookies = document.cookie;
+  }
+
+  // get and store the color mode from the cookies.
+  // It'll update the cookies if there isn't any and we have set a default value
+  let colorMode = useMemo(() => {
+    let color = getColorMode(cookies);
+
+    if (!color && DEFAULT_COLOR_MODE) {
+      cookies += ` ${CHAKRA_COOKIE_COLOR_KEY}=${DEFAULT_COLOR_MODE}`;
+      color = DEFAULT_COLOR_MODE;
+    }
+
+    return color;
+  }, [cookies]);
+
   return (
-    <html lang="en" className="h-full">
+    <html
+      lang="en"
+      {...(colorMode && {
+        "data-theme": colorMode,
+        style: { colorScheme: colorMode },
+      })}
+    >
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
+        {serverStyleData?.map(({ key, ids, css }) => (
+          <style
+            key={key}
+            data-emotion={`${key} ${ids.join(" ")}`}
+            dangerouslySetInnerHTML={{ __html: css }}
+          />
+        ))}
       </head>
-      <body className="h-full">
-        <Outlet />
+      <body
+        {...(colorMode && {
+          className: `chakra-ui-${colorMode}`,
+        })}
+      >
+        <ChakraProvider
+          colorModeManager={cookieStorageManagerSSR(cookies)}
+          theme={theme}
+        >
+          <Outlet />
+        </ChakraProvider>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
   );
-}
+});
+
+export default App;
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,27 @@
+import { Button, useColorMode } from "@chakra-ui/react";
 import type { V2_MetaFunction } from "@remix-run/node";
+import { LightBulbIcon } from "@heroicons/react/24/solid";
 
 export const meta: V2_MetaFunction = () => [{ title: "Home" }];
 
 export default function Index() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <main className="relative min-h-screen bg-white ">
-      <h1>Akash Agarwal</h1>
+    <main className="relative flex min-h-screen min-w-full font-sans">
+      <section className="mb-60 flex min-h-full w-full flex-col items-center justify-center">
+        <h1 className="text-6xl uppercase tracking-widest">Akash Agarwal</h1>
+        <p>Senior Front End Developer</p>
+        <div className="fixed right-0 top-0">
+          <Button onClick={toggleColorMode} className="m-3">
+            {colorMode === "light" ? (
+              <LightBulbIcon className="h-6 w-6" />
+            ) : (
+              <LightBulbIcon className="h-6 w-6" />
+            )}
+          </Button>
+        </div>
+      </section>
     </main>
   );
 }
