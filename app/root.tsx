57140e1911cfd94b8ec6871cb9c7a51ebd5ce03d
Akash Agarwal <akash191095@gmail.com>
2023-04-30 20:32:12 +0530

    Fix missing viewport tag

--- app/root.tsx
+++ app/root.tsx
@@ -101,6 +101,8 @@ const App = withEmotionCache(({ children }: DocumentProps, emotionCache) => {
       })}
     >
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
         {serverStyleData?.map(({ key, ids, css }) => (
